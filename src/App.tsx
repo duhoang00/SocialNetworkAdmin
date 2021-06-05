8336288cc5105bf975efc236207c1849f6b6cb98
Dzu <hoangminhdu2000@gmail.com>
2021-06-05 15:34:25 +0700

    eslint added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
 import { PostContainer } from "./components/PostContainer";
 import { UserContainer } from "./components/UserContainer";
